266438b809e66f9bb60f45fb9cba77a76b9b046e
Antoine Alberti <aa@antoinealberti.com>
2016-04-05 19:06:37 +0200

    Get stats
    Change-Id: I3fac9f51bb6535e150df2b5ab577c5c14b6b3c2f

--- src/main/java/com/aa/angularboot/controllers/SomeController.java
+++ src/main/java/com/aa/angularboot/controllers/SomeController.java
@@ -6,7 +6,10 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collection;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,6 +25,70 @@ public class SomeController {
             for (int i = 0; i < columnNames.length; i++)
                 columnNames[i] = metaData.getColumnName(i + 1);
             return columnNames;
+        }
+    }
+
+    @RequestMapping("/stats")
+    public Collection<Stat> statsGroupedBy(@RequestParam(value="column") String columnName) throws SQLException{
+        ArrayList<Stat> result = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:us-census.db")) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select \"" + columnName + "\", avg(age) as avgAge, count(age) as count "
+                + "from census_learn_sql "
+                + "group by \"" + columnName + "\" "
+                + "order by count desc "
+                + "limit 100");
+            while (resultSet.next()) {
+                int count = resultSet.getInt("count");
+                if (count > 0)
+                    result.add(new Stat(resultSet.getString(columnName), resultSet.getFloat("avgAge"), count));
+            }
+        }
+        return result;
+    }
+
+    @SuppressWarnings("unused")
+    public static class Stat
+    {
+        private String classifier;
+        private float  avgAge;
+        private int    count;
+
+        private Stat(String classifier, float avgAge, int count)
+        {
+            this.classifier = classifier;
+            this.avgAge = avgAge;
+            this.count = count;
+        }
+
+        public String getClassifier()
+        {
+            return classifier;
+        }
+
+        public void setClassifier(String classifier)
+        {
+            this.classifier = classifier;
+        }
+
+        public float getAvgAge()
+        {
+            return avgAge;
+        }
+
+        public void setAvgAge(float avgAge)
+        {
+            this.avgAge = avgAge;
+        }
+
+        public int getCount()
+        {
+            return count;
+        }
+
+        public void setCount(int count)
+        {
+            this.count = count;
         }
     }
 }
--- src/main/webapp/js/application.js
+++ src/main/webapp/js/application.js
@@ -1,17 +1,16 @@
 var app = angular.module('angularBoot', [])
-app.controller('statsCtrl', function($scope) {
-    $scope.name = 'anybody'
-    $scope.rows = [
-          {field: 'toto', count: 12},
-          {field: 'Chuck Norris what did you expect', count: 42}
-    ]
-})
-
-app.controller('fieldCtrl', function($scope, $rootScope, $http) {
+app.controller('statsCtrl', function($scope, $rootScope, $http) {
     $http.get('/fields').then(function(response) {
         $scope.fields = response.data
     })
     $scope.selectField = function(field) {
         $rootScope.selectedField = field
+        $http({
+            method: 'GET',
+            url: '/stats',
+            params: { column: field }
+        }).then(function(response) {
+           $rootScope.stats = response.data
+        })
     }
 })
